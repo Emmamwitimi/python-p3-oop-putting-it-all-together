da5f3b36052e0f147cbf3f21198454bf2866089f
Emmaculate <emmamwitimi14@gmail.com>
2024-09-03 14:40:20 +0300

    complete lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size  # This will use the setter method
+    
+    # Getter method for size
+    def get_size(self):
+        return self._size
+    
+    # Setter method for size with validation
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    # Define size property
+    size = property(get_size, set_size)
+    
+    # Method to repair the shoe
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
